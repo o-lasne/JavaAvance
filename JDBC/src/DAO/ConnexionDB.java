c00f487ef2264c97d7324e358223cb63e65f90a4
Cynthia-Thaina <33545636+Cynthia-Thaina@users.noreply.github.com>
2019-01-11 16:33:41 +0100

    [ADD] preparedStatement function

--- JDBC/src/DAO/ConnexionDB.java
+++ JDBC/src/DAO/ConnexionDB.java
@@ -1,57 +1,65 @@
-package DAO;
+package dao;
 
 import java.sql.*;	                 // ResultSet Connection DriverManager SQLException Statement
 
-	public class ConnexionDB{
 
+public class ConnexionDB{
+
+	/**
+	 Charge le pilote jdbc
+	 */
 	public static void main(String[] args) {
-	try{
-
-	Class.forName("com.mysql.jdbc.Driver");
-	System.out.println("Pilote chargÈ");
-
-	}catch(ClassNotFoundException e){
-
-	System.out.println("Pilote non trouvÈ. "+e.getMessage());
+		try{		
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Pilote charg√©");
+		}
+		
+		catch(ClassNotFoundException e){
+			System.out.println("Pilote non trouv√©. "+e.getMessage());
+		}
+	
+		/*****************************************************/
+	
+		String url="jdbc:mysql://localhost/exempleBDD"; //url de la base 
+		ResultSet res = null;
+		Connection cnx=null;
+		PreparedStatement preparedStatement = null;
+		
+		try{
+	
+			cnx = DriverManager.getConnection(url,"root","");  //identifiant, mot de passe 
+			//System.out.println("connection √©tablie avec succ√®s.\n"); 
+			//Statement stmt = cnx.createStatement();
+			
+			//requette pour r√©cup les notes <10
+			String requete = " SELECT * FROM bulletin where note < ?;";
+			preparedStatement = cnx.prepareStatement(requete);
+			preparedStatement.setInt(1,10); 
+			res = preparedStatement.executeQuery();
+				
+				
+			System.out.println("Le resultat de la requete ["+requete+"] est :"); 
+			while(res.next()){
+			
+				
+			//Affichage des notes			
+			System.out.print(res.getString(2)+" ");
+			
+			System.out.println(res.getString(3)+" ");
+			
+			System.out.println(res.getString(4)+" ");
+			}
+			
+			System.out.println("\n Affichage termin√©.");
+			
+			cnx.close(); //fermeture de la connexion
+			
+		}
+	
+		catch(SQLException e){
+			
+			  System.out.println("Erreur de connexion ou d'ex√©cution de la requ√™te"+   
+			  e.getMessage());
+		}
 	}
-
-	/*****************************************************/
-
-	String url="jdbc:mysql://localhost/exempleBDD"; //url de la base 
-	ResultSet res = null;
-
-	String requete = "select * from matiere";     //‡ crÈer díabord la table 
-	Connection cnx=null;
-
-	try{
-
-	cnx = DriverManager.getConnection(url,"root","");  //identifiant, mot de passe 
-	System.out.println("connection Ètablie avec succËs.\n"); 
-	Statement stmt = cnx.createStatement();
-
-	res = stmt.executeQuery(requete);
-
-	System.out.println("Le resultat de la requete ["+requete+"] est :"); 
-	while(res.next()){
-
-	System.out.print(res.getString(1)+" ");
-
-	System.out.print(res.getString(2)+" ");
-
-	System.out.println(res.getString(3));
-	}
-
-	System.out.println("\n Affichage terminÈ.");
-
-	cnx.close(); //fermeture de la connexion
-
-	}
-	catch(SQLException e){
-
-	         System.out.println("Erreur de connexion ou d'exÈcution de la requÍte"+   
-	         e.getMessage());
-	}
-	}
-	}
-
-
+}
